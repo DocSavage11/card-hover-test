d26018bef6ccd1b4e292c27acf235913dd45efed
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-15 11:03:33 -0500

    Added information. Styling it next.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <Poster
       img= {require("./Assest/Imagies/Rear-Window-Poster.jpg")}
+      title="Rear Window"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi magni, quidem asperiores itaque numquam ab pariatur aliquid ipsum quas maxime mollitia quasi! Cupiditate tempora facere tenetur dolores asperiores. Necessitatibus."
     />
   );
 }
