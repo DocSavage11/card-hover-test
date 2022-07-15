49348ba9ec381037308cc0d75c60b91960574768
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-15 11:57:17 -0500

    Added more posters

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import Poster from './Components/Poster';
 
 function App() {
   return (
-    <Poster
+    <>
+      <Poster
       img= {require("./Assest/Imagies/Rear-Window-Poster.jpg")}
       title="Rear Window"
       description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi magni, quidem asperiores itaque numquam ab pariatur aliquid ipsum quas maxime mollitia quasi! Cupiditate tempora facere tenetur dolores asperiores. Necessitatibus."}
     />
+    <Poster
+      img= {require("./Assest/Imagies/American-Psycho-Poster.jpg")}
+      title="Rear Window"
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi magni, quidem asperiores itaque numquam ab pariatur aliquid ipsum quas maxime mollitia quasi! Cupiditate tempora facere tenetur dolores asperiores. Necessitatibus."}
+    />
+    <Poster
+      img= {require("./Assest/Imagies/Parasite-Poster.jpg")}
+      title="Rear Window"
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi magni, quidem asperiores itaque numquam ab pariatur aliquid ipsum quas maxime mollitia quasi! Cupiditate tempora facere tenetur dolores asperiores. Necessitatibus."}
+    />
+    <Poster
+      img= {require("./Assest/Imagies/Star-Wars-ESB-Poster.jpg")}
+      title="Rear Window"
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi magni, quidem asperiores itaque numquam ab pariatur aliquid ipsum quas maxime mollitia quasi! Cupiditate tempora facere tenetur dolores asperiores. Necessitatibus."}
+    />
+    </>
   );
 }
 
